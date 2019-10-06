7957d4ae431ab734c8e46eebdf2bd795bce12c3f
mmmries <riesmmm@gmail.com>
2019-10-06 15:10:52 -0600

    initial

---
+++ lib/glider.ex
@@ -0,0 +1,18 @@
+defmodule Glider do
+  @moduledoc """
+  Documentation for Glider.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Glider.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/glider_test.exs
@@ -0,0 +1,8 @@
+defmodule GliderTest do
+  use ExUnit.Case
+  doctest Glider
+
+  test "greets the world" do
+    assert Glider.hello() == :world
+  end
+end
