2c3cd5bce068acf5140247f1eb054d6a8c4daff7
mmmries <riesmmm@gmail.com>
2019-12-01 21:42:59 -0700

    extract the circle logic

--- lib/glider/heading_feedback.ex
+++ lib/glider/heading_feedback.ex
@@ -13,9 +13,11 @@ defmodule Glider.HeadingFeedback do
             servo_center: 1500,
             servo_range: 300
 
+  alias Glider.Circle
+
   def servo_pulsewidth(heading, feedback) do
-    heading = wrap(heading)
-    diff = difference_from_desired(heading, feedback) |> clamp(feedback)
+    heading = Circle.wrap(heading)
+    diff =  Circle.difference(feedback.desired, heading) |> clamp(feedback)
     servo_offset = :erlang.round((diff / feedback.sensor_range) * feedback.servo_range)
     case feedback.reversed do
       false -> feedback.servo_center - servo_offset
@@ -26,21 +28,4 @@ defmodule Glider.HeadingFeedback do
   defp clamp(diff, %{sensor_range: range}) when diff > range, do: range
   defp clamp(diff, %{sensor_range: range}) when diff < (-1 * range), do: -1 * range
   defp clamp(diff, _feedback), do: diff
-
-  @max_angle 360.0
-  defp wrap(heading) when heading > @max_angle do
-    heading - @max_angle
-  end
-  defp wrap(heading) when heading < 0.0 do
-    @max_angle + heading
-  end
-  defp wrap(heading), do: heading
-
-  defp difference_from_desired(heading, %__MODULE__{desired: desired}) do
-    case desired - heading do
-      diff when diff < -180.0 -> diff + 360.0
-      diff when diff > 180.0 -> diff - 360.0
-      diff -> diff
-    end
-  end
 end
