3e881436a7b8a4b861b9b10c157a425cbd9e3cde
mmmries <riesmmm@gmail.com>
2020-02-04 21:44:46 -0700

    wire up the new Elevon code

--- lib/glider.ex
+++ lib/glider.ex
@@ -1,72 +1,40 @@
 defmodule Glider do
   use GenServer
   require Logger
-  alias Glider.{Circle,HeadingFeedback,PitchFeedback}
+  alias Glider.Elevon
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
-  @pause_between_sensors_reads 5
-  @desired_heading 135.0
 
   def init(opts) do
     bno055 = BNO055.setup(:NDOF)
-    pitch = %PitchFeedback{
-      offset: -1.0,
-      reversed: false,
-      sensor_ceiling: 20.0,
-      sensor_floor: -20.0,
-      servo_max: 2000,
-      servo_min: 1500
-    }
-    heading = %HeadingFeedback{
-      desired: 0.0,
-      reversed: true,
-      max_roll: 15.0,
-      servo_center: 1575,
-      servo_range: 300
+    elevon = %Elevon{
+      desired_heading: 0.0,
+      desired_pitch: 0.0,
+      left_center: 1500,
+      left_direction: -1,
+      right_center: 1500,
+      right_direction: 1,
     }
     opts = opts
       |> Keyword.put(:bno055, bno055)
-      |> Keyword.put(:heading, heading)
-      |> Keyword.put(:pitch, pitch)
+      |> Keyword.put(:elevon, elevon)
     send self(), :refresh
     {:ok, opts}
   end
 
-  @elevator_pin 18
-  @heading_pin 13
+  @pause_between_sensors_reads 50
+  @left_pin 18
+  @right_pin 13
   def handle_info(:refresh, state) do
     {:ok, orientation} = Keyword.get(state, :bno055) |> BNO055.orientation()
-    %{heading: heading, pitch: pitch, roll: roll} = orientation
+    {left, right} = state |> Keyword.get(:elevon) |> Elevon.feedback(orientation)
 
-    if reading_reasonable?(pitch) do
-      pitch_feedback = Keyword.get(state, :pitch)
-      pulsewidth = PitchFeedback.servo_pulsewidth(pitch, pitch_feedback)
-      Pigpiox.GPIO.set_servo_pulsewidth(@elevator_pin, pulsewidth)
-    end
-
-    if reading_reasonable?(heading) and reading_reasonable?(roll) do
-      heading_feedback = Keyword.get(state, :heading) |> update_desired_heading(heading)
-      Logger.info("Current Heading #{heading}, Desired: #{heading_feedback.desired}")
-      pulsewidth = HeadingFeedback.servo_pulsewidth(heading, roll, heading_feedback)
-      Pigpiox.GPIO.set_servo_pulsewidth(@heading_pin, pulsewidth)
-    end
+    Pigpiox.GPIO.set_servo_pulsewidth(@left_pin, left)
+    Pigpiox.GPIO.set_servo_pulsewidth(@right_pin, right)
 
     Process.send_after(self(), :refresh, @pause_between_sensors_reads)
     {:noreply, state}
-  end
-
-  defp reading_reasonable?(reading) when reading < -180.0, do: false
-  defp reading_reasonable?(reading) when reading > 390.0, do: false
-  defp reading_reasonable?(_reading), do: true
-
-  defp update_desired_heading(feedback, heading) do
-    diff = Circle.difference(@desired_heading, heading)
-    if abs(diff) < 90.0 do
-      %{feedback | desired: @desired_heading}
-    else
-      %{feedback | desired: Circle.wrap(@desired_heading + 180.0)}
-    end
   end
 end
