e330e1eb7b632db0557e3482dbb5427316f98894
mmmries <riesmmm@gmail.com>
2020-02-06 23:34:03 -0700

    tuning

--- lib/glider.ex
+++ lib/glider.ex
@@ -2,6 +2,7 @@ defmodule Glider do
   use GenServer
   require Logger
   alias Glider.Elevon
+  alias BNO055.Smoothing
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -11,30 +12,44 @@ defmodule Glider do
     bno055 = BNO055.setup(:NDOF)
     elevon = %Elevon{
       desired_heading: 0.0,
-      desired_pitch: 0.0,
-      left_center: 1500,
+      desired_pitch: -4.0,
+      left_center: 1430,
       left_direction: -1,
-      right_center: 1500,
+      right_center: 1470,
       right_direction: 1,
     }
     opts = opts
       |> Keyword.put(:bno055, bno055)
       |> Keyword.put(:elevon, elevon)
+      |> Keyword.put(:smoothing, Smoothing.init())
     send self(), :refresh
     {:ok, opts}
   end
 
-  @pause_between_sensors_reads 50
-  @left_pin 18
-  @right_pin 13
+  @pause_between_sensors_reads 33
+  @left_pin 13
+  @right_pin 18
   def handle_info(:refresh, state) do
     {:ok, orientation} = Keyword.get(state, :bno055) |> BNO055.orientation()
-    {left, right} = state |> Keyword.get(:elevon) |> Elevon.feedback(orientation)
+    state = if orientation_reasonable?(orientation) do
 
-    Pigpiox.GPIO.set_servo_pulsewidth(@left_pin, left)
-    Pigpiox.GPIO.set_servo_pulsewidth(@right_pin, right)
+      {smoothing, orientation} = Keyword.get(state, :smoothing) |> Smoothing.reading(orientation)
+      {left, right} = state |> Keyword.get(:elevon) |> Elevon.feedback(orientation)
+
+      Pigpiox.GPIO.set_servo_pulsewidth(@left_pin, left)
+      Pigpiox.GPIO.set_servo_pulsewidth(@right_pin, right)
+      Keyword.put(state, :smoothing, smoothing)
+    else
+      Logger.error("Invalid Orientation Data")
+      state
+    end
 
     Process.send_after(self(), :refresh, @pause_between_sensors_reads)
+
     {:noreply, state}
+  end
+
+  def orientation_reasonable?(%{heading: heading, pitch: pitch, roll: roll}) do
+    heading > -20.0 and heading <= 380.0 and pitch >= -200.0 and pitch <= 200.0 and roll >= -200.0 and roll <= 200.0
   end
 end
--- lib/glider/elevon.ex
+++ lib/glider/elevon.ex
@@ -13,17 +13,17 @@ defmodule Glider.Elevon do
   @doc "represent how far the nose is up-or-down in terms of a servo pulsewidth offset"
   def vertical_bias(%__MODULE__{desired_pitch: desired}, %{pitch: actual}) do
     diff = clamp(actual - desired, 30.0)
-    {diff * 10, diff * 10}
+    {diff * 15, diff * 15}
   end
 
-  @roll_ratio 0.15 # a ratio of how much roll we should have for a given heading difference
+  @roll_ratio 1.0 # a ratio of how much roll we should have for a given heading difference
   @max_roll 20.0 # the maximum roll angle we'll sustain
   def roll_bias(%__MODULE__{desired_heading: desired}, %{roll: roll, heading: heading}) do
     heading = Circle.wrap(heading)
     heading_diff = Circle.difference(desired, heading)
     desired_roll = (heading_diff * @roll_ratio) |> clamp(@max_roll)
     roll_diff = clamp(roll - desired_roll, 30.0)
-    {roll_diff * 10, roll_diff * -10}
+    {roll_diff * -15, roll_diff * 15}
   end
 
   defp clamp(number, max), do: clamp(number, -1 * max, max)
